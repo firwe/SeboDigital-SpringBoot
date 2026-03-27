d493e0b6e5cf147e2a63f4a396eb91902f939c65
joaosouza <joao.asouzs@gmail.com>
2026-03-27 04:45:03 -0300

    atualizacao site, separando css e ajustando barra de pesquisa de categorias

--- src/main/java/br/com/sebodigital/sebodigital/web/controller/AjudaController.java
+++ src/main/java/br/com/sebodigital/sebodigital/web/controller/AjudaController.java
@@ -1,4 +1,12 @@
 package br.com.sebodigital.sebodigital.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AjudaController {
+    @GetMapping("/ajuda")
+    public String ajuda() {
+        return "categories/ajuda";
+    }
 }
--- target/classes/static/js/scripts.js
+++ target/classes/static/js/scripts.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    // 1. Carrega o Header
+    // 1. Header
     fetch('header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
             
-            // Depois que o header carregar, verifica o login
             verificarEstadoLogin();
         });
 
-    // 2. Carrega o Footer
+    // 2. Footer
     fetch('footer.html')
         .then(response => response.text())
         .then(data => {
@@ -18,21 +17,15 @@ document.addEventListener("DOMContentLoaded", function() {
         });
 });
 
-// Função que muda a cara do Header se o usuário estiver logado
 function verificarEstadoLogin() {
-    // Lê a 'memória' do navegador
     const usuarioLogado = localStorage.getItem('logado');
 
     if (usuarioLogado === 'true') {
-        // Pega os elementos do HTML pelo ID
         const textoConta = document.getElementById('texto-conta');
         const linkConta = document.getElementById('btn-conta');
         
-        // Altera para a visão de logado
         if (textoConta) textoConta.innerText = "Minha Conta";
-        if (linkConta) linkConta.href = "#"; // Aqui você colocaria o link do perfil do usuário
-        
-        // Opcional: Você pode mudar a imagem do ícone também
-        // document.getElementById('icone-conta').src = "link-do-novo-icone-aqui";
+        if (linkConta) linkConta.href = "#";
+
     }
 }
